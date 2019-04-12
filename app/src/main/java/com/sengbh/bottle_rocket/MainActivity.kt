570b5e0d148062afe73f82df23e1416383e4c71f
sengbh <sengbh@ieee.org>
2019-04-12 18:09:03 -0500

    modified vers

--- app/src/main/java/com/sengbh/bottle_rocket/MainActivity.kt
+++ app/src/main/java/com/sengbh/bottle_rocket/MainActivity.kt
@@ -3,10 +3,13 @@ package com.sengbh.bottle_rocket
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.LinearLayout
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,25 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ProductAdapter()
         }
     }
 }
---
+++ app/src/main/java/com/sengbh/bottle_rocket/ProductAdapter.kt
@@ -0,0 +1,6 @@
+package com.sengbh.bottle_rocket
+
+import android.support.v7.widget.RecyclerView
+
+class ProductAdapter : RecyclerView.Adapter<ProductAdapter> {
+}
