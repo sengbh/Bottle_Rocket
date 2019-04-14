658404d9428e0a33b79da3b5896999167a20003b
sengbh <sengbh@ieee.org>
2019-04-14 02:56:41 -0500

    mod version

--- app/src/main/java/com/sengbh/bottle_rocket/MainActivity.kt
+++ app/src/main/java/com/sengbh/bottle_rocket/MainActivity.kt
@@ -1,28 +1,50 @@
 package com.sengbh.bottle_rocket
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.provider.ContactsContract
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.LinearLayout
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 private lateinit var linearLayoutManager: LinearLayoutManager
+private lateinit var adapter: RecycleViewAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private val photosList: ArrayList<ContactsContract.CommonDataKinds.Photo> = null!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         linearLayoutManager = LinearLayoutManager(this)
         recycler_view.layoutManager = linearLayoutManager
-
-
-        setSupportActionBar(toolbar)
+        adapter = RecycleViewAdapter(this.photosList)
+        recycler_view.adapter = adapter
     }
+
+    override fun onStart() {
+        super.onStart()
+        if (photosList.size == 0)
+            requePhoto()
+    }
+
+    private fun requePhoto() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun receiveNewPhoto(newPhoto: ContactsContract.Contacts.Photo){
+        runOnUiThread{
+            photosList.add(newPhoto)
+            adapter.notifyItemInserted(photosList.size-1)
+        }
+    }
+}
+
+private fun <E> java.util.ArrayList<E>.add(newPhoto: ContactsContract.Contacts.Photo) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
--- app/src/main/java/com/sengbh/bottle_rocket/RecycleViewAdapter.kt
+++ app/src/main/java/com/sengbh/bottle_rocket/RecycleViewAdapter.kt
@@ -1,45 +1,83 @@
 package com.sengbh.bottle_rocket
 
+import android.content.Intent
 import android.provider.ContactsContract
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.recycleview_table.view.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 //private val photos: ArrayList<Photo> = null
 
-class RecycleViewAdapter(private val photos: ArrayList<ContactsContract.CommonDataKinds.Photo>) : RecyclerView.Adapter<RecycleViewAdapter.PhotoHolder>() {
+private val ContactsContract.CommonDataKinds.Photo.url: Any
+    get() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://sandbox.bottlerocketapps.com/BR_Android_CodingExam_2015_Server/stores.json")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-    abstract override fun getItemCount(): photos.size
+        val service = retrofit.create(ParsingRetrofit::class.java)
 
-    override fun onBindViewHolder(holder: RecycleViewAdapter.PhotoHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+class RecycleViewAdapter(private val photos: ArrayList<ContactsContract.CommonDataKinds.Photo>) : RecyclerView.Adapter<RecycleViewAdapter.PhotoHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecycleViewAdapter.PhotoHolder {
-       val inflateView = parent.inflate(R.layout.recycleview_table, false)
+        val inflateView = parent.inflate(R.layout.recycleview_table, false)
         return PhotoHolder(inflateView)
+    }
+
+    override fun onBindViewHolder(holder: RecycleViewAdapter.PhotoHolder, position: Int) {
+        val itemPhoto = photos[position]
+        holder.bindPhoto(itemPhoto)
+
+    }
+
+    override fun getItemCount() = photos.size
+
+
+    class PhotoHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        private var view: View = v
+        private var photo: ContactsContract.CommonDataKinds.Photo? = null
+
+        fun bindPhoto(photo: ContactsContract.CommonDataKinds.Photo) {
+            this.photo = photo
+            var into: Any = Picasso.get().load(photo.url).into(view.itemImage)
+            view.itemDate.text = photo.toString()
+            view.itemDescription.text = photo.toString()
+        }
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            val context = itemView.context
+            val showPhotoIntent = Intent(context, PhotoActivity::class.java)
+            showPhotoIntent.putExtra(PHOTO_KEY, photo)
+            context.startActivity(showPhotoIntent)
+            Log.d("RecyclerView", "CLICK!")
+        }
+
+        companion object {
+            private const val PHOTO_KEY = "PHOTO"
+        }
     }
 }
 
-//1
-class PhotoHolder (v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
-    //2
-    private var view: View = v
-    private var photo: ContactsContract.CommonDataKinds.Photo? = null
+private fun Any.into(itemImage: ImageView?): Any {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
 
-    //3
-    init {
-        v.setOnClickListener(this)
-    }
+private fun Picasso.load(url: Any): Any {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
 
-    //4
-    override fun onClick(v: View) {
-        Log.d("RecyclerView", "CLICK!")
-    }
+private fun Intent.putExtra(photO_KEY: String, photo: ContactsContract.CommonDataKinds.Photo?) {
 
-    companion object {
-        //5
-        private val PHOTO_KEY = "PHOTO"
-    }
 }
